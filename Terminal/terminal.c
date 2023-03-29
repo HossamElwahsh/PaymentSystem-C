e2daac8ff90d1341594fb385fca79cd5f1ab652c
TarekGohry <telgohry@gmail.com>
2023-03-29 06:02:48 +0200

    Created terminal.c file structure

---
+++ Terminal/terminal.c
@@ -0,0 +1,28 @@
+/**
+ ****************************************************************************************
+ * @file           : terminal.c
+ * @author         : Team 1
+ * @brief          : Contains the functionality of terminal
+ ****************************************************************************************
+ */
+
+ typedef struct ST_terminalData_t
+ {
+     float transAmount;
+     float maxTransAmount;
+     uint8_t transactionDate[11];
+
+ }ST_terminalData_t;
+
+ typedef enum
+
+
+
+
+ /**
+ ****************************************************************************************
+ User                Date                 Brief
+ ****************************************************************************************
+ Tarek Gohry         29MARCH2023          Task-1 implement getTransactionDate Function
+
+*/
