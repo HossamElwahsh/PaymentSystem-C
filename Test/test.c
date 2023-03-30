944af5ea7e71aed3c54cee1afe9c683abf23be9a
Hossam Elwahsh <hacker2894@gmail.com>
2023-03-30 04:21:49 +0200

    refactor: old comments

--- Test/test.c
+++ Test/test.c
@@ -22,74 +22,8 @@ void getTestFileName(int i, char * test_dir, char * testFilename, uint8_t fileTy
 /***
  * Runs test cases for getCardPan
  */
-/*
+
 void getCardPANTest(void)
-{
-    // test init
-    int testCasesCount = 3;
-    char test_expected_result_filename[50];
-    char * test_dir = CONCAT(TEST_DIR, "getCardPan.csv");
-
-    // test case data
-    char inputData[25];
-    char expectedResult[100];
-
-    ST_cardData_t * cardData = calloc(1, sizeof(ST_cardData_t));
-
-    // Print Test Header
-    printf("===============================\n");
-    printf("Tester Name:\tHossam Elwahsh\n");
-    printf("Function Name:\tgetCardPan\n");
-    printf("===============================\n");
-
-    // running test cases
-    for (int i = 0; i < testCasesCount; ++i) {
-        printf("\n-----------------------\n");
-        printf("Test Case %d\n", i+1);
-        printf("-----------------------\n");
-        FILE *fp_input_data;
-        FILE *fp_expected_result;
-
-
-        // redirect test case inputData to stdin
-        fp_input_data = freopen(test_dir, "r", stdin);
-
-        // read test case inputData
-        fgets(inputData, sizeof(inputData), fp_input_data);
-        rewind(fp_input_data);
-
-        // print inputData data
-        printf("Input Data: %s\n", inputData);
-
-        // read and print expected result
-        getTestFileName(i, test_dir, test_expected_result_filename, 0);
-        fp_expected_result = fopen(test_expected_result_filename, "r");
-        fgets(expectedResult, sizeof(expectedResult), fp_expected_result);
-        printf("Expected Result: %s\n", expectedResult);
-        fclose(fp_expected_result);
-
-        */
-/************* Execute test case ***************//*
-
-        EN_cardError_t ret = getCardPAN(cardData);
-        // turn on console logs
-
-        printf("Actual Result: ");
-        switch (ret) {
-            case CARD_OK:
-                printf("CARD OK\n");
-                break;
-            case WRONG_PAN:
-                printf("WRONG_PAN\n");
-                break;
-        }
-
-        fclose(fp_input_data);
-    }
-}
-*/
-
-void getCardPANTest_Fix(void)
 {
     // test cases init
     char * test_cases_filename = CONCAT(TEST_DIR, "getCardPan.csv");
@@ -177,11 +111,6 @@ void getCardPANTest_Fix(void)
 
         fclose(fp_test_cases);*/
 //    }
-}
-
-int main()
-{
-    getCardPANTest_Fix();
 }
 
 
@@ -326,7 +255,7 @@ void testAll()
 {
     /** CARD MODULE */
     // todo print card module testing
-//    getCardPANTest();
+    getCardPANTest();
 
     /** TERMINAL MODULE */
 //     todo print terminal module testing
